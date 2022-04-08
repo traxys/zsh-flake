ff5ec7b45f48e22aebf833ef4cc4a404fe7331cf
Quentin Boyer <quentin.boyer@atos.net>
2022-04-08 12:09:05 +0200

    enable nix-index

--- flake.nix
+++ flake.nix
@@ -39,69 +39,75 @@
         };
       in
       {
-        programs.zsh = {
-          enable = true;
-          enableCompletion = true;
-          oh-my-zsh = {
+        programs = {
+          nix-index = {
             enable = true;
-            plugins = [ "git" "wd" "rust" ];
+            enableZshIntegration = true;
           };
-          plugins = [
-            {
-              name = "fast-syntax-highlighting";
-              file = "fast-syntax-highlighting.plugin.zsh";
-              src = inputs.fast-syntax-highlighting;
-            }
-            {
-              name = "zsh-nix-shell";
-              file = "nix-shell.plugin.zsh";
-              src = inputs.zsh-nix-shell;
-            }
-            {
-              name = "nix-zsh-completions";
-              file = "nix-zsh-completions.plugin.zsh ";
-              src = inputs.nix-zsh-completions;
-            }
-          ];
-          initExtra =
-            ''
-              export PATH="$PATH:$HOME/bin"
-              source ~/.p10k.zsh
-              source ~/.powerlevel10k/powerlevel10k.zsh-theme
-              eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
-              if [ -f "$HOME/.zvars" ]; then
-                source "$HOME/.zvars"
-              fi
+          zsh = {
+            enable = true;
+            enableCompletion = true;
+            oh-my-zsh = {
+              enable = true;
+              plugins = [ "git" "wd" "rust" ];
+            };
+            plugins = [
+              {
+                name = "fast-syntax-highlighting";
+                file = "fast-syntax-highlighting.plugin.zsh";
+                src = inputs.fast-syntax-highlighting;
+              }
+              {
+                name = "zsh-nix-shell";
+                file = "nix-shell.plugin.zsh";
+                src = inputs.zsh-nix-shell;
+              }
+              {
+                name = "nix-zsh-completions";
+                file = "nix-zsh-completions.plugin.zsh ";
+                src = inputs.nix-zsh-completions;
+              }
+            ];
+            initExtra =
+              ''
+                export PATH="$PATH:$HOME/bin"
+                source ~/.p10k.zsh
+                source ~/.powerlevel10k/powerlevel10k.zsh-theme
+                eval "$(${pkgs.direnv}/bin/direnv hook zsh)"
+                if [ -f "$HOME/.zvars" ]; then
+                  source "$HOME/.zvars"
+                fi
 
-              if [ -f "$HOME/.localrc.sh" ]; then
-                source "$HOME/.localrc.sh"
-              fi
+                if [ -f "$HOME/.localrc.sh" ]; then
+                  source "$HOME/.localrc.sh"
+                fi
 
-              export PATH="${config.home.homeDirectory}/bin:$PATH"
+                export PATH="${config.home.homeDirectory}/bin:$PATH"
 
-              ${pkgs.fortune}/bin/fortune \
-                | ${pkgs.cowsay}/bin/cowsay \
-                | ${dotacat}/bin/dotacat
-            '';
-          shellAliases = {
-            cat = "${pkgs.bat}/bin/bat -p";
-            ls = "${pkgs.exa}/bin/exa --icons";
-            py3 = "nix-shell -p python3 python3.pkgs.matplotlib --run python3";
+                ${pkgs.fortune}/bin/fortune \
+                  | ${pkgs.cowsay}/bin/cowsay \
+                  | ${dotacat}/bin/dotacat
+              '';
+            shellAliases = {
+              cat = "${pkgs.bat}/bin/bat -p";
+              ls = "${pkgs.exa}/bin/exa --icons";
+              py3 = "nix-shell -p python3 python3.pkgs.matplotlib --run python3";
+            };
           };
-        };
 
-        home.file = {
-          ".powerlevel10k".source = inputs.powerlevel10k;
-          ".zprofile".source = ./zprofile;
-          ".p10k.zsh".source = ./p10k.zsh;
-          "bin/ns" = {
-            text = ''
-              #!/usr/bin/env bash
+          home.file = {
+            ".powerlevel10k".source = inputs.powerlevel10k;
+            ".zprofile".source = ./zprofile;
+            ".p10k.zsh".source = ./p10k.zsh;
+            "bin/ns" = {
+              text = ''
+                #!/usr/bin/env bash
 
-              set -- "''${@/#/nixpkgs#}"
-              nix shell "$@"
-            '';
-            executable = true;
+                set -- "''${@/#/nixpkgs#}"
+                nix shell "$@"
+              '';
+              executable = true;
+            };
           };
         };
       };
